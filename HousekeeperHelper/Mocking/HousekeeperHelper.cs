f1fcb77c9847ccd026b92f6d6969c11b0410d44d
RossaMania <raclettenberg@gmail.com>
2024-07-07 20:50:37 -0500

    Refactor HousekeeperHelper to use dependency injection for email sending. Create EmailSender class and interface.

--- HousekeeperHelper/Mocking/HousekeeperHelper.cs
+++ HousekeeperHelper/Mocking/HousekeeperHelper.cs
@@ -1,8 +1,5 @@
 using System;
 using System.IO;
-using System.Net;
-using System.Net.Mail;
-using System.Text;
 
 namespace HousekeeperHelperProject.Mocking
 {
@@ -10,10 +7,11 @@ namespace HousekeeperHelperProject.Mocking
     {
         private readonly IUnitOfWork _unitOfWork;
 
-        public HousekeeperHelper(IUnitOfWork unitOfWork, IStatementGenerator statementGenerator)
+        public HousekeeperHelper(IUnitOfWork unitOfWork, IStatementGenerator statementGenerator, IEmailSender emailSender)
         {
             _unitOfWork = unitOfWork;
             _statementGenerator = statementGenerator;
+            _emailSender = emailSender;
         }
 
         public bool SendStatementEmails(DateTime statementDate)
@@ -37,7 +35,7 @@ namespace HousekeeperHelperProject.Mocking
                 // It will try to email statement to housekeeper. If it fails, it will show a message box
                 try
                 {
-                    EmailFile(emailAddress, emailBody, statementFilename,
+                    _emailSender.EmailFile(emailAddress, emailBody, statementFilename,
                         string.Format("Sandpiper Statement {0:yyyy-MM} {1}", statementDate, housekeeper.FullName));
                 }
                 catch (Exception e)
@@ -50,36 +48,6 @@ namespace HousekeeperHelperProject.Mocking
             return true;
         }
 
-
-        private static void EmailFile(string emailAddress, string emailBody, string filename, string subject)
-        {
-            var client = new SmtpClient(SystemSettingsHelper.EmailSmtpHost)
-            {
-                Port = SystemSettingsHelper.EmailPort,
-                Credentials =
-                    new NetworkCredential(
-                        SystemSettingsHelper.EmailUsername,
-                        SystemSettingsHelper.EmailPassword)
-            };
-
-            var from = new MailAddress(SystemSettingsHelper.EmailFromEmail, SystemSettingsHelper.EmailFromName,
-                Encoding.UTF8);
-            var to = new MailAddress(emailAddress);
-
-            var message = new MailMessage(from, to)
-            {
-                Subject = subject,
-                SubjectEncoding = Encoding.UTF8,
-                Body = emailBody,
-                BodyEncoding = Encoding.UTF8
-            };
-
-            message.Attachments.Add(new Attachment(filename));
-            client.Send(message);
-            message.Dispose();
-
-            File.Delete(filename);
-        }
     }
 
     public enum MessageBoxButtons
